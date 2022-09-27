7d2654951962b9cb6a5d36d74a814db0a3e648cc
kanai <c0b21045eb@edu.teu.ac.jp>
2022-09-27 17:20:13 +0900

    未完成のゲーム

---
+++ ex01/game.py
@@ -0,0 +1,49 @@
+import random
+import random
+global a, b, c, alpha
+a = 9
+b = 2
+c = 3
+alpha = 26
+
+def shutudai(alpha):
+    lst = []
+    prom = []
+    for i in range(alpha -1):
+        lst.append(chr(i+65))
+    random.shuffle(lst)
+    del lst[0:24-a]
+    for w in lst:
+        print(f"対象文字:{w}")
+    delst = []
+    for i in range(b):
+        D = lst.pop()
+        delst.append(D)
+    print(f"欠損文字:{delst}")
+    print(f"表示文字:{lst}")
+    return delst
+
+def kaitou(seikai):
+    ans = int(input("欠損文字はいくつあるでしょうか？:"))
+    if ans != b:
+        print("不正解です")
+    else:
+        print("正解です、では具体的に欠損文字を1つずつ入力してください")
+        for i in range(ans):
+            c= 
+
+if __name__ == "__main__":
+    C = 0
+    while C != c:
+        A = shutudai()
+        B = kaitou(A)
+        if B == 4:
+            C += 1
+
+
+    
+
+
+
+
+
