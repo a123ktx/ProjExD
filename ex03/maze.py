8635d9e7a15dae9814724dc99223b2e8bffc708c
kanai <c0b21045eb@edu.teu.ac.jp>
2022-10-18 14:11:58 +0900

    3回目練習2

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,5 +3,8 @@ import tkinter as tk
 if __name__ == "__main__":
 
     root = tk.Tk()
-    root.title("迷えるこうかとん")
+    root.title("迷えるこうかとん")#練習1
+
+    canv = tk.Canvas(root, width=1500, height=900, bg="black")
+    canv.pack()
     root.mainloop()
