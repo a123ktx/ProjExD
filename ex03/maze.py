128c0baf74a8232d9007a985ee9163903f399733
kanai <c0b21045eb@edu.teu.ac.jp>
2022-10-18 16:30:26 +0900

    gameover追加

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,8 +3,11 @@ import maze_maker as mm
 
 #key_down関数を定義する
 def key_down(event):
-    global key
+    global key, tmr_S
     key = event.keysym
+    if tmr_S == False:
+        root.after(100, count_down)
+        tmr_S = True
 
 #key_up関数を定義する
 def key_up(event):
@@ -15,6 +18,7 @@ def key_up(event):
 def main_proc():
     global mx, my
     global cx, cy
+    global game_C
     if key == "Up":
         my -=1
     elif key == "Down":
@@ -34,11 +38,32 @@ def main_proc():
             mx -= 1
         elif key == "Left":
             mx += 1
-            
     canv.coords("tori", cx, cy)
-    root.after(100, main_proc)
+    if game_C == True:
+        root.after(100, main_proc)
+
+def count_down():#カウントダウンする関数を定義する
+    global tmr, game_C
+    tmr -= 1
+    canv.create_rectangle(0, 0, 150, 100, outline ="white", fill="white")
+    canv.create_text(100,50, text=tmr, font=("", 80))
+    root.after(1000, count_down)
+    if tmr < 0:
+        canv.create_text(800, 400, text="GAME OVER", font=("",80), fill="red")
+        game_C = False
+        
+
+
 
 if __name__ == "__main__":
+    #タイマーの初期値を設定する
+    tmr = 30
+    #タイマーが動いてるかどうか設定する
+    tmr_S = False
+
+    #ゲームが続いているのか確認する
+    game_C = True
+
     #ウィンドウを生成する
     root = tk.Tk()
     root.title("迷えるこうかとん")#練習1
@@ -50,6 +75,10 @@ if __name__ == "__main__":
     #mmモジュールの関数を呼び出す
     maze_list = mm.make_maze(15, 9)
     maze_show = mm.show_maze(canv, maze_list)
+
+    #残り時間を表示する
+    label = tk.Label(root, font=("", 80))
+    label.pack()
 
     #こうかとんを表示する
     tori = tk.PhotoImage(file="ex03/fig/4.png")
