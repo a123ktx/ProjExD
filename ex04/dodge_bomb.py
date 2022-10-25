cef5852d938d4f25b4e98f9972ad3dd772d2b317
kanai <c0b21045eb@edu.teu.ac.jp>
2022-10-25 14:39:17 +0900

    練習4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -9,17 +9,31 @@ def main():
     bg_sfc = py.image.load("ex04/haikei.jpg")
     bg_rct = bg_sfc.get_rect()
 
+    #練習3
+    tori_sfc = py.image.load("fig/6.png")
+    tori_sfc = py.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+
     clock = py.time.Clock()
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct) #練習2
-        py.display.update()
-        clock.tick(1000)              #1000fpsを刻む
 
         for event in py.event.get():  #イベントを繰り返しで処理
             if event.type == py.QUIT: #ウィンドウの×ボタンをクリックしたら
                 return #main関数を抜け出す
 
+        key_states = py.key.get_pressed()
+        if key_states[py.K_UP]: tori_rct.centery -= 1  #こうかとんが縦に+1
+        if key_states[py.K_DOWN]: tori_rct.centery += 1 #こうかとんが縦に-1
+        if key_states[py.K_LEFT]: tori_rct.centerx -= 1 #こうかとんが横に-1
+        if key_states[py.K_RIGHT]: tori_rct.centerx += 1 #こうかとんが横に-1
 
+        scrn_sfc.blit(tori_sfc, tori_rct)
+        py.display.update()
+        clock.tick(1000)              #1000fpsを刻む
+
+                
 if __name__ == "__main__":
     py.init()  #モジュールを初期化する
     main()     #ゲームの本体
